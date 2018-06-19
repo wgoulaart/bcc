02df787a8369a8e6ecce7396080e943f6b3805b6
William Goulart <williamgoulart1@hotmail.com>
2018-06-19 00:49:39 -0400

    [ADD] Creating tests to validate options params of Currency & Amount values

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -17,7 +17,24 @@ describe('Main CLI', () => {
   it('should return the description when bcc --help', done => {
     exec(`${bcc} --help`, (err, stdout, stderr) => {
       if (err) throw err
-      expect(stdout.includes('Convert Bitcoin to any currency defined')).to.be.true
+      expect(stdout.includes('Convert Bitcoin to any currency defined')).to.be
+        .true
+      done()
+    })
+  })
+
+  it('should return the currency option when bcc --help', done => {
+    exec(`${bcc} --help`, (err, stdout, stderr) => {
+      if (err) throw err
+      expect(stdout.includes('--currency')).to.be.true
+      done()
+    })
+  })
+
+  it('should return the amount option when bcc --help', done => {
+    exec(`${bcc} --help`, (err, stdout, stderr) => {
+      if (err) throw err
+      expect(stdout.includes('--amount')).to.be.true
       done()
     })
   })
