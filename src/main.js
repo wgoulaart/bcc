9fd6419b5756ef5803f5d00e8440226d928469f3
William Goulart <williamgoulart1@hotmail.com>
2018-06-18 23:27:04 -0400

    [ADD] Creating first test version BCC :fire:

--- src/main.js
+++ src/main.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
-console.log('Hello');
+const program = require('commander')
+
+program
+  .version('1.0.0')
+  .parse(process.argv)
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,15 +1,14 @@
-const expect = require('chai').expect;
-const exec   = require('child_process').exec;
-const bcc    = './src/main.js';
+const expect = require('chai').expect
+
+const exec = require('child_process').exec
+const bcc = 'node.exe ./src/main.js'
 
 describe('Main CLI', () => {
-  it('should return Hello', (done) => {
-    exec(bcc, (err, stdout, stderr) => {
-      if (err) throw err;
-
-      expect(stdout.replace('\n', '')).to.be.equal('Hello');
-
-      done();
-    });
-  });
-});
+  it('should return version of bcc', done => {
+    exec(`${bcc} --version`, (err, stdout, stderr) => {
+      if (err) throw err
+      expect(stdout.replace('\n', '')).to.be.equal('1.0.0')
+      done()
+    })
+  })
+})
